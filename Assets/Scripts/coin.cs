1331a66101058c40072fff2a58f577dfc7c945be
Kyle <rankakuhub@users.github.com>
2020-03-06 10:46:30 +0000

    second commit (likely last - needs tidy)

---
+++ Assets/Scripts/coin.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coin : MonoBehaviour
+
+{
+    
+   
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            ScoreTextScript.coinAmount += 1;
+            
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/playsound.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playsound : MonoBehaviour
+{
+  
+    public AudioSource audio;
+    private bool hasPLayed = false;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            AudioSource audio = GetComponent<AudioSource>();
+            audio.Play();
+            hasPLayed = true;
+            Destroy(gameObject, 1f);
+        }
+        
+    }
+}
